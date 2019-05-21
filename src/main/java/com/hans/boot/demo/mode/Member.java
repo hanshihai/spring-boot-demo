7110609553abc2eb2cb90df657b9e101037f39e7
hans <shi-hai.han@dxc.com>
2019-05-21 16:28:29 +0800

    enable sample service

---
+++ src/main/java/com/hans/boot/demo/mode/Member.java
@@ -0,0 +1,30 @@
+package com.hans.boot.demo.mode;
+
+public class Member {
+
+    private long id;
+    private String name;
+
+    public Member() {}
+
+    public Member(long id, String name) {
+        this.id = id;
+        this.name = name;
+    }
+
+    public long getId() {
+        return id;
+    }
+
+    public void setId(long id) {
+        this.id = id;
+    }
+
+    public String getName() {
+        return name;
+    }
+
+    public void setName(String name) {
+        this.name = name;
+    }
+}
