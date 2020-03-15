f51dcff222b5af9b47851d403eb85a3a7008e988
Hans Han <hans.han2@medtronic.com>
2020-03-15 22:00:51 +0800

    add zip download

--- src/main/java/com/hans/boot/demo/MainResource.java
+++ src/main/java/com/hans/boot/demo/MainResource.java
@@ -26,5 +26,65 @@ public class MainResource {
         }
     }
 
+    @RequestMapping(value="/mediaEntries/tests", method = RequestMethod.POST,
+            consumes = MediaType.APPLICATION_FORM_URLENCODED_VALUE)
+    public ResponseEntity<List<Long>> checkParameters(@RequestParam("entryIds") Long[] entryIds) {
+        List<Long> result = new ArrayList<>();
+        Arrays.stream(entryIds).forEach(id -> result.add(id));
+        return ResponseEntity.ok(result);
+    }
+
+    @RequestMapping(value="/mediaEntries/tests", method = RequestMethod.POST,
+            consumes = MediaType.APPLICATION_JSON_VALUE)
+    public ResponseEntity<List<Long>> checkParameters(@RequestBody Long[] entryIds) {
+        List<Long> result = new ArrayList<>();
+        Arrays.stream(entryIds).forEach(id -> result.add(id));
+        return ResponseEntity.ok(result);
+    }
+
+    @GetMapping("/mediaEntries/zipfiles")
+    public StreamingResponseBody downloadZipFiles(HttpServletResponse response) throws FileNotFoundException {
+        logger.info("got here: downloadFiles for entry ");
+
+        /*MediaEntry mediaEntry = mediaEntriesRepository.findOne(entryId);
+
+        if (mediaEntry == null) {
+            throw new NotFoundException("MediaEntry is not found");
+        }*/
+
+        response.setContentType("application/x-msdownload;charset=UTF-8");
+        response.setHeader("Content-Disposition", "attachment; filename=\"downloads.zip\"");
+
+        InputStream[] inputStreams = new InputStream[3];
+        InputStream input1 = new FileInputStream(new File("C:/repository/prints/passort-recipe.pdf"));
+        InputStream input2 = new FileInputStream(new File("C:/repository/elpworkspace/.metadata/version.ini"));
+        inputStreams[0] = input1;
+        inputStreams[1] = input2;
+        inputStreams[2] = input1;
+
+        String[] names = new String[] {"passort-recipe.pdf", "version.ini", "version.ini"};
+        //InputStream inputStream = storageService.loadFile(mediaEntry);
+
+        return new StreamingResponseBody() {
+            @Override
+            public void writeTo(OutputStream outputStream) throws IOException {
+                ZipOutputStream zipOutpuStream = new ZipOutputStream(outputStream);
+                int index = 0;
+                byte[] data = new byte[1024];
+                for(InputStream inputStream : inputStreams) {
+                    zipOutpuStream.putNextEntry(new ZipEntry(""+index+"/"+names[index]));
+                    int nRead;
+                    while ((nRead = inputStream.read(data, 0, data.length)) != -1) {
+                        zipOutpuStream.write(data, 0, nRead);
+                    }
+                    zipOutpuStream.flush(); //notice it impacts the close entry or not?
+                    zipOutpuStream.closeEntry();
+                    index++;
+                }
+                zipOutpuStream.close();
+            }
+        };
+    }
+
 }
 
